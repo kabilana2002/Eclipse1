fa6bc3ae253fc19aee44a161003f78b38bd89e35
kabilan <kabilana2002@gmail.com>
2024-11-25 14:44:57 +0530

    Branch 1.1

--- SimpleProj1/src/demo/Main.java
+++ SimpleProj1/src/demo/Main.java
@@ -3,7 +3,7 @@ package demo;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Life is beautiful, is it sooper");
+		System.out.println("Life is beautiful, is it");
 
 	}
 
